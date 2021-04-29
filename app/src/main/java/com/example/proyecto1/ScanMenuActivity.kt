9e5f518710da2d2f19f952809d399c91bdc12a02
Juan Cla C <juanclaudiocarrascotapia@gmail.com>
2021-04-29 01:15:37 -0400

    Commit Diseño Pant. Inf

--- app/src/main/java/com/example/proyecto1/ScanMenuActivity.kt
+++ app/src/main/java/com/example/proyecto1/ScanMenuActivity.kt
@@ -3,18 +3,14 @@ package com.example.proyecto1
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_scan_menu.*
 
-class RegisterActivity : AppCompatActivity() {
+class ScanMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-        btnBack.setOnClickListener {
+        setContentView(R.layout.activity_scan_menu)
+        btnBack2.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-        btnInfo.setOnClickListener{
-            val intent = Intent(this, ScanMenuActivity::class.java)
             startActivity(intent)
         }
     }
