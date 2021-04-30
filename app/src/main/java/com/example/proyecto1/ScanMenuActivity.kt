6953b9b1245decc26bc6cc04cd7487e25d22d1b5
Juan Cla C <juanclaudiocarrascotapia@gmail.com>
2021-04-29 23:14:03 -0400

    Commit Diseño Pant. Inf 2

--- app/src/main/java/com/example/proyecto1/ScanMenuActivity.kt
+++ app/src/main/java/com/example/proyecto1/ScanMenuActivity.kt
@@ -2,6 +2,7 @@ package com.example.proyecto1
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_scan_menu.*
 
@@ -9,9 +10,20 @@ class ScanMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_menu)
-        btnBack2.setOnClickListener{
+        btnBack2.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        val Departamentos = arrayOf("CH", "LP", "CB", "OR", "PT", "TJ", "SC", "BE", "PD")
+        val ArrayDps =
+            ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, Departamentos)
+        spinnerDepts.adapter = ArrayDps
+
+        val lugaresLP = arrayOf("UMSA", "UNIVALLE", "ProSalud")
+        val lugaresCB = arrayOf("Sennfeld", "Pacata", "ProSalud", "COMBASE")
+
+                val arrayLgs =
+                    ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, lugaresLP)
+                spinnerLugaresVac.adapter = arrayLgs
     }
 }
